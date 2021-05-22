e6e1e4502c4cf5cdb5b0062e0af9a0241340273f
pschraut <p-github@console-dev.de>
2021-05-22 18:26:09 +0200

    clamp displayTime to avoid entering negative numbers. make sure its entire label is visible. changed other toggle settings to properly align with displayTime.

--- Editor/MiniEditorIterationProfilerSettings.cs
+++ Editor/MiniEditorIterationProfilerSettings.cs
@@ -2,6 +2,7 @@
 // Mini Editor Iteration Profiler for Unity. Copyright (c) 2020 Peter Schraut (www.console-dev.de). See LICENSE.md
 // https://github.com/pschraut/UnityMiniEditorIterationProfiler
 //
+using UnityEngine;
 using UnityEditor;
 
 namespace Oddworm.EditorFramework
@@ -19,12 +20,17 @@ namespace Oddworm.EditorFramework
                 {
                     EditorGUILayout.Space();
 
+                    var labelWidth = EditorGUIUtility.labelWidth;
+                    EditorGUIUtility.labelWidth = EditorGUIUtility.currentViewWidth * 0.25f;
+
                     EditorGUI.indentLevel++;
-                    MiniEditorIterationProfiler.showCompilationAndAssemblyReload = EditorGUILayout.ToggleLeft("Show Compilation and Assembly Reload", MiniEditorIterationProfiler.showCompilationAndAssemblyReload);
-                    MiniEditorIterationProfiler.showEnterPlayMode = EditorGUILayout.ToggleLeft("Show Enter Play Mode", MiniEditorIterationProfiler.showEnterPlayMode);
-                    MiniEditorIterationProfiler.showExitPlayMode = EditorGUILayout.ToggleLeft("Show Exit Play Mode", MiniEditorIterationProfiler.showExitPlayMode);
-                    MiniEditorIterationProfiler.displayTime = EditorGUILayout.FloatField("Message Display Time (seconds)", MiniEditorIterationProfiler.displayTime);
+                    MiniEditorIterationProfiler.showCompilationAndAssemblyReload = EditorGUILayout.Toggle("Show Compilation and Assembly Reload", MiniEditorIterationProfiler.showCompilationAndAssemblyReload);
+                    MiniEditorIterationProfiler.showEnterPlayMode = EditorGUILayout.Toggle("Show Enter Play Mode", MiniEditorIterationProfiler.showEnterPlayMode);
+                    MiniEditorIterationProfiler.showExitPlayMode = EditorGUILayout.Toggle("Show Exit Play Mode", MiniEditorIterationProfiler.showExitPlayMode);
+                    MiniEditorIterationProfiler.displayTime = Mathf.Clamp(EditorGUILayout.FloatField("Message Display Time (seconds)", MiniEditorIterationProfiler.displayTime), 0, 60);
                     EditorGUI.indentLevel--;
+
+                    EditorGUIUtility.labelWidth = labelWidth;
                 },
             };
 
